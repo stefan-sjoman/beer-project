ebbf1c7d1d7e806f1296bff1ae1a0a2dc70ee92d
RebeckaOscarsson <70890347+RebeckaOscarsson@users.noreply.github.com>
2020-11-04 09:55:16 +0100

    worked on render function

--- beer-info.js
+++ beer-info.js
@@ -4,34 +4,42 @@ const id = searchParams.get('name');
 const url = `${api}/${id}`;
 const mainElement = document.querySelector('main');
 const infoList = document.getElementById('info-list');
-const infoHeader= document.querySelector('h1');
-const newLi = document.createElement('li');
-const image= document.querySelector('img');
+const infoHeader = document.querySelector('h1');
+
+const image = document.querySelector('img');
 
 
 getData(url, render);
 
 function getData(url, callback) {
-    
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
 
-        callback(data);
-    })
-    .catch(error => console.log(error));
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+
+            callback(data);
+        })
+        .catch(error => console.log(error));
 }
 
+
+
 function render(data) {
-    
     const beer = data[0];
-    let infoArray=[beer.description, beer.abv, beer.volume, beer.ingredients, beer.brewers_tips];
-    infoArray.forEach((item, index)=>{newLi.textContent = item[index]})//Här var vi
+    let keysArray = ["Description: ", "Hops: ", "Alcohol per volume: ", "Volume: ", "Ingredients: ", "Brewers tips: ", "Food pairing: "]
+    let hops = beer.ingredients.hops.name//här var vi
+    let valuesArray = [beer.description, beer.ingredients.hops, beer.abv, beer.volume, beer.ingredients, beer.brewers_tips, beer.food_pairing];
+    valuesArray.forEach((item, index) => {console.log(item)
+        let newLi = document.createElement('li');
+        newLi.textContent = keysArray[index] + item;
+        infoList.appendChild(newLi);
+    })
+
     const name = beer.name;
     infoHeader.textContent = name;
+
+
     
-    infoList.appendChild(newLi);
-    console.log(beer)
 }
 
 
